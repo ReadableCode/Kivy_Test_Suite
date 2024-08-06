e75a2cb00a90d244034a739c9ad82a09f85aa431
jason <jasonwchristiansen@gmail.com>
2024-08-05 21:47:59 -0500

    update docs and script for trial on android

--- README.md
+++ README.md
@@ -81,6 +81,12 @@ Kivy Test Suite
   source $(pipenv --venv)/bin/activate
   ```
 
+  On Windows (Powershell):
+
+  ```bash
+  & "$(pipenv --venv)\Scripts\activate.ps1"
+  ```
+
 * To Deactivate:
 
   ```bash
@@ -96,6 +102,28 @@ buildozer init
 Edit the buildozer.spec file to include the following:
 
 ```ini
+[app]
+title = My Kivy App
+package.name = mykivyapp
+package.domain = org.test
+source.dir = src
+source.include_exts = py,png,jpg,kv,atlas
+version = 0.1
+requirements = python3,kivy
+orientation = portrait
+osx.python_version = 3
+osx.kivy_version = 1.9.1
+fullscreen = 0
+android.archs = arm64-v8a
+android.allow_backup = True
+ios.kivy_ios_url = https://github.com/kivy/kivy-ios
+ios.kivy_ios_branch = master
+ios.ios_deploy_url = https://github.com/phonegap/ios-deploy
+ios.ios_deploy_branch = 1.10.0
+ios.codesign.allowed = false
+[buildozer]
+log_level = 2
+warn_on_root = 1
 ```
 
 ```bash
@@ -115,3 +143,10 @@ If run into an issue with `.buildozer/android/platform/android-sdk/tools/bin/sdk
   ```bash
   buildozer -v android debug
   ```
+
+If run into other errors, clean and rebuild
+
+```bash
+buildozer android clean
+buildozer android debug
+```
--- src/main.py
+++ src/main.py
@@ -1,11 +1,59 @@
+import os
+import re
+
+import kivy
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.label import Label
 
+kivy.require("2.1.0")
 
-class MyApp(App):
+
+class WifiPasswordApp(App):
     def build(self):
-        return Label(text="Hello, Kivy!")
+        self.layout = BoxLayout(orientation="vertical")
+        self.label = Label(
+            text="Click the button to get Wi-Fi password", font_size="20sp"
+        )
+        self.button = Button(text="Get Wi-Fi Password", on_press=self.get_wifi_password)
+        self.layout.add_widget(self.label)
+        self.layout.add_widget(self.button)
+        return self.layout
+
+    def get_wifi_password(self, instance):
+        ssid, password = self.read_wifi_password()
+        if ssid and password:
+            self.label.text = f"SSID: {ssid}\nPassword: {password}"
+        else:
+            self.label.text = (
+                "Could not retrieve Wi-Fi password. Make sure your device is rooted."
+            )
+
+    def read_wifi_password(self):
+        # Path to the wpa_supplicant.conf file
+        wifi_conf_path = "/data/misc/wifi/wpa_supplicant.conf"
+
+        if not os.path.exists(wifi_conf_path):
+            return None, None
+
+        ssid = None
+        password = None
+
+        with open(wifi_conf_path, "r") as f:
+            content = f.read()
+
+            # Regex to find SSID and PSK (pre-shared key / password)
+            ssid_match = re.search(r'ssid="([^"]+)"', content)
+            password_match = re.search(r'psk="([^"]+)"', content)
+
+            if ssid_match:
+                ssid = ssid_match.group(1)
+            if password_match:
+                password = password_match.group(1)
+
+        return ssid, password
 
 
 if __name__ == "__main__":
-    MyApp().run()
+    WifiPasswordApp().run()
